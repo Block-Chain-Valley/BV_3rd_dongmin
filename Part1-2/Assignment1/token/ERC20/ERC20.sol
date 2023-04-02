a1c6c50db1af71ce7a66db0b029acddeee2a78b8
toto9602 <toto9602@naver.com>
2023-04-02 23:51:12 +0900

    [feat] ERC20

--- Part1-2/Assignment1/token/ERC20/ERC20.sol
+++ Part1-2/Assignment1/token/ERC20/ERC20.sol
@@ -42,11 +42,144 @@ contract ERC20 is Context, IERC20, IERC20Metadata {
     string private _name;
     string private _symbol;
 
+    modifier isValidAddress(address account) {
+        require(account != address(0), "is Zero Address");
+        _;
+    }
+
+    modifier isAllValidAddress(address accountA, address accountB) {
+        require(accountA != address(0), "is Zero Address");
+        require(accountB != address(0), "is Zero Address");
+        _;
+    }
+
+    function name() external view virtual override returns (string memory) {
+        return _name;
+    }
+
+    function symbol() external view virtual override returns (string memory) {
+        return _symbol;
+    }
+
+    function decimals() external view virtual override returns (uint8) {
+        return 18;
+    }
+
     function totalSupply() external view returns (uint256) {
         return _totalSupply;
     }
 
     function balanceOf(address account) external view returns (uint256) {
         return _balances[account];
+    }
+
+    function _transfer(
+        address from,
+        address to,
+        uint256 amount
+    ) internal virtual isAllValidAddress(from, to) {
+        uint256 senderBalance = _balances[from];
+
+        // 검증 과정을 생략하고 연산
+        unchecked {
+            _balances[from] = senderBalance - amount;
+            _balances[to] += amount;
+        }
+
+        emit Transfer(from, to, amount);
+    }
+
+    function _mint(
+        address account,
+        uint256 amount
+    ) internal virtual isValidAddress(account) {
+        _totalSupply += amount;
+        _balances[account] += amount;
+    }
+
+    function _burn(
+        address account,
+        uint256 amount
+    ) internal virtual isValidAddress(account) {
+        unchecked {
+            _balances[account] -= amount;
+        }
+        _totalSupply -= amount;
+    }
+
+    function _approve(
+        address owner,
+        address spender,
+        uint256 amount
+    ) internal virtual isAllValidAddress(owner, spender) {
+        _allowances[owner][spender] = amount;
+        emit Approval(owner, spender, amount);
+    }
+
+    function _spendAllowance(
+        address owner,
+        address spender,
+        uint256 amount
+    ) internal virtual isAllValidAddress(owner, spender) {
+        uint256 ownerAllowance = _allowances[owner][spender];
+
+        _allowances[owner][spender] = ownerAllowance - amount;
+    }
+
+    function transfer(
+        address recipient,
+        uint256 amount
+    ) external virtual override returns (bool) {
+        _transfer(_msgSender(), recipient, amount);
+        return true;
+    }
+
+    function allowance(
+        address owner,
+        address spender
+    ) external view virtual override returns (uint256) {
+        return _allowances[owner][spender];
+    }
+
+    function approve(
+        address spender,
+        uint256 amount
+    ) external virtual override returns (bool) {
+        _approve(_msgSender(), spender, amount);
+        return true;
+    }
+
+    function transferFrom(
+        address sender,
+        address recipient,
+        uint256 amount
+    ) external virtual override returns (bool) {
+        _spendAllowance(sender, _msgSender(), amount);
+        _transfer(sender, recipient, amount);
+        return true;
+    }
+
+    function increaseAllowance(
+        address spender,
+        uint256 addedValue
+    ) external virtual returns (bool) {
+        _approve(
+            _msgSender(),
+            spender,
+            _allowances[_msgSender()][spender] + addedValue
+        );
+        return true;
+    }
+
+    function decreaseAllowance(
+        address spender,
+        uint256 subtractedValue
+    ) external virtual returns (bool) {
+        _approve(
+            _msgSender(),
+            spender,
+            _allowances[_msgSender()][spender] - subtractedValue
+        );
+        return true;
     }
 }
